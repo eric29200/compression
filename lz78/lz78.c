2d15a117592e77a3aa435dd87bd6c5ce89c48936
eric29200 <eric_begot@yahoo.fr>
2024-02-02 16:25:12 +0100

    fix lz78

--- lz78/lz78.c
+++ lz78/lz78.c
@@ -44,6 +44,29 @@ static void __grow_buffer(uint8_t **dst, uint8_t **buf_out, uint32_t *dst_capaci
 }
 
 /**
+ * @brief Decode a node.
+ * 
+ * @param node 		node
+ * @param buf_out 	output buffer
+ * 
+ * @return number of characters written
+ */
+static int __decode_node(struct trie *node, uint8_t *buf_out)
+{
+	struct trie *tmp;
+	int i, j;
+
+	/* compute dict entry size */
+	for (tmp = node, i = 0; tmp->parent != NULL; tmp = tmp->parent, i++);
+
+	/* write decoded string */
+	for (tmp = node, j = 0; tmp->parent != NULL; tmp = tmp->parent, j++)
+		buf_out[i - 1 - j] = tmp->val;
+
+	return i;
+}
+
+/**
  * @brief Compress a buffer with LZ78 algorithm.
  * 
  * @param src 		input buffer
@@ -74,7 +97,7 @@ uint8_t *lz78_compress(uint8_t *src, uint32_t src_len, uint32_t *dst_len)
 	*((int *) buf_out) = htole32(id);
 	buf_out += sizeof(int);
 
-	/* insert root node */
+	/* create root node */
 	root = node = trie_insert(NULL, 0, id++);
 
 	/* create dictionnary */
@@ -137,9 +160,9 @@ uint8_t *lz78_compress(uint8_t *src, uint32_t src_len, uint32_t *dst_len)
  */
 uint8_t *lz78_uncompress(uint8_t *src, uint32_t src_len, uint32_t *dst_len)
 {
-	int dict_size, id = 0, node_id, i, j;
-	struct trie **dict, *root, *node, *tmp;
 	uint8_t *dst, *buf_in, *buf_out, c;
+	int dict_size, id = 0, node_id, i;
+	struct trie **dict, *root, *node;
 
 	/* read uncompressed length first */
 	*dst_len = le32toh(*((uint32_t *) src));
@@ -173,15 +196,8 @@ uint8_t *lz78_uncompress(uint8_t *src, uint32_t src_len, uint32_t *dst_len)
 		/* get node */
 		node = dict[node_id];
 
-		/* compute dict entry size */
-		for (tmp = node, i = 0; tmp->parent != NULL; tmp = tmp->parent, i++);
-
-		/* write decoded string */
-		for (tmp = node, j = 0; tmp->parent != NULL; tmp = tmp->parent, j++)
-			buf_out[i - 1 - j] = tmp->val;
-		
-		/* update output buffer position */
-		buf_out += i;
+		/* decode node */
+		buf_out += __decode_node(node, buf_out);
 
 		/* no next character : exit */
 		if (buf_in >= src + src_len)
